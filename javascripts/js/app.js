02608d1821b5bd575d9acc83c9471a90db345732
Nicholas Castaneda <npcastaneda@gmail.com>
2016-02-09 18:16:29 -0800

    Initialization. Routes set and about me draft

---
+++ javascripts/js/app.js
@@ -0,0 +1,24 @@
+angular.module('NickCastaneda', ['ui.router'])
+  .config(MainRouter)
+
+function MainRouter($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "partials/home.html"
+    })
+    .state('about', {
+      url: "/",
+      templateUrl: "partials/about-me.html"
+    })
+    .state('projects', {
+      url: "/",
+      templateUrl: "partials/projects.html"
+    })
+    .state('contact', {
+      url: "/",
+      templateUrl: "partials/contact.html"
+    })
+
+  $urlRouterProvider.otherwise("/");
+}
